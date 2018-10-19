5f62fe35cfe700592e99a16b2c772bacf757e480
Gara64 <paul@cozycloud.cc>
2018-10-19 12:37:43 +0200

    Fix tests

--- src/photos/ducks/clustering/services.spec.js
+++ src/photos/ducks/clustering/services.spec.js
@@ -2,9 +2,11 @@ import {
   computeTemporalEps,
   computeSpatialEps,
   computeSpatioTemporalScaledEps,
+  computeSpatioTemporalMaxNormalizedEps,
   runOptics
 } from './services'
 import Metrics from './metrics'
+import { gradientClustering, gradientAngle } from './gradient'
 
 const N_DIGITS = 4
 const dataset = [
@@ -41,6 +43,7 @@ const dataset = [
 ]
 
 const metric = new Metrics()
+const eps = 1000
 
 describe('knn', () => {
   it('Should compute temporal eps', () => {
@@ -50,45 +53,82 @@ describe('knn', () => {
     expect(computeSpatialEps(dataset, metric)).toBeCloseTo(386.7568, N_DIGITS)
   })
   it('Should compute spatio temporal scaled eps', () => {
-    console.log('metric : ', metric)
     expect(computeSpatioTemporalScaledEps(dataset, metric)).toBeCloseTo(
       10.2669,
+      N_DIGITS
+    )
+  })
+  it('Should compute spatio temporal max normalized eps', () => {
+    expect(computeSpatioTemporalMaxNormalizedEps(dataset, metric)).toBeCloseTo(
+      1.0381,
       N_DIGITS
     )
   })
 })
 
 describe('optics', () => {
-  it('Should compute reachabilities with temporal metric', () => {
-    const expected = [[0, undefined], [1, 2], [2, 3], [3, 45], [4, 20], [5, 30]]
-    expect(runOptics(dataset, 1000, metric.temporal)).toEqual(
-      expect.arrayContaining(expected)
-    )
-  })
-  it('Should compute reachabilities with spatial metric', () => {
-    const expected = [
-      [0, undefined],
-      [1, 11.119492664455889],
-      [4, 244.9352126291911],
-      [3, 68.80809741870237],
-      [2, 386.7568104542303],
-      [5, undefined]
+  it('Should cluster data with temporal metric', () => {
+    const expectedOrder = [0, 1, 2, 3, 4, 5]
+    const expectedReach = [undefined, 2, 3, 45, 20, 30]
+    const expectedClusters = [
+      [dataset[0], dataset[1], dataset[2]],
+      [dataset[3], dataset[4], dataset[5]]
     ]
-    expect(runOptics(dataset, 1000, metric.spatial)).toEqual(
-      expect.arrayContaining(expected)
-    )
+
+    const optics = runOptics(dataset, eps, metric.temporal)
+    console.log('optics : ', optics)
+    expect(optics.ordering).toEqual(expect.arrayContaining(expectedOrder))
+    expect(optics.reachabilities).toEqual(expect.arrayContaining(expectedReach))
+    const angle = gradientAngle(15, 1)
+    const clustering = gradientClustering(dataset, optics, angle, eps)
+    expect(clustering).toEqual(expect.arrayContaining(expectedClusters))
   })
-  it('Should compute reachabilities with temporal metric', () => {
-    const expected = [
-      [0, undefined],
-      [1, 1.0431259089583833],
-      [2, 3.0571814094974092],
-      [3, 24],
-      [4, 10.266865749582626],
-      [5, 25.831530037137995]
+
+  it('Should cluster data with spatial metric', () => {
+    const expectedOrder = [0, 1, 4, 3, 2, 5]
+    const expectedReach = [
+      undefined,
+      11.119492664456596,
+      386.7568104542309,
+      68.80809741870237,
+      244.93521262919106,
+      undefined
     ]
-    expect(runOptics(dataset, 1000, metric.spatioTemporalScaled)).toEqual(
-      expect.arrayContaining(expected)
-    )
+    const expectedClusters = [
+      [dataset[0], dataset[1], dataset[2], dataset[3], dataset[4]],
+      [dataset[5]]
+    ]
+
+    const optics = runOptics(dataset, eps, metric.spatial)
+    console.log('optics : ', optics)
+    expect(optics.ordering).toEqual(expect.arrayContaining(expectedOrder))
+    expect(optics.reachabilities).toEqual(expect.arrayContaining(expectedReach))
+    const angle = gradientAngle(15, 1)
+    const clustering = gradientClustering(dataset, optics, angle, eps)
+    expect(clustering).toEqual(expect.arrayContaining(expectedClusters))
+  })
+
+  it('Should cluster data with spatio temporal scaled metric', () => {
+    const expectedOrder = [0, 1, 2, 3, 4, 5]
+    const expectedReach = [
+      undefined,
+      1.043125908958386,
+      3.057181409497407,
+      24,
+      10.266865749582625,
+      25.831530037137977
+    ]
+    const expectedClusters = [
+      [dataset[0], dataset[1], dataset[2]],
+      [dataset[3], dataset[4], dataset[5]]
+    ]
+
+    const optics = runOptics(dataset, eps, metric.spatioTemporalScaled)
+    console.log('optics : ', optics)
+    expect(optics.ordering).toEqual(expect.arrayContaining(expectedOrder))
+    expect(optics.reachabilities).toEqual(expect.arrayContaining(expectedReach))
+    const angle = gradientAngle(15, 1)
+    const clustering = gradientClustering(dataset, optics, angle, eps)
+    expect(clustering).toEqual(expect.arrayContaining(expectedClusters))
   })
 })
