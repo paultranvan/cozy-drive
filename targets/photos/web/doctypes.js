03893fc7f0e513b297e6ecb980ddb6f17eaed541
Gara64 <paul@cozycloud.cc>
2018-10-30 15:20:06 +0100

    Query albums through files

--- targets/photos/web/doctypes.js
+++ targets/photos/web/doctypes.js
@@ -1,3 +1,31 @@
+import { QueryDefinition, HasMany } from 'cozy-client'
+
+class HasManyAlbums extends HasMany {
+
+  get data() {
+    const refs = this.target.relationships.referenced_by.data
+    const albums = refs
+      ? refs.map(ref => this.get(ref.type, ref.id)).filter(Boolean)
+      : []
+    return albums
+  }
+
+  static query(doc, client, assoc) {
+    if (
+      !doc['relationships'] ||
+      !doc['relationships']['referenced_by'] ||
+      !doc['relationships']['referenced_by']['data']) {
+      return null
+    }
+    const included = doc['relationships']['referenced_by']['data']
+    const ids = included
+      .filter(inc => inc.type === assoc.doctype)
+      .map(inc => inc.id)
+
+    return new QueryDefinition({ doctype: assoc.doctype, ids })
+  }
+}
+
 export default {
   albums: {
     doctype: 'io.cozy.photos.albums',
@@ -15,6 +43,12 @@ export default {
     }
   },
   files: {
-    doctype: 'io.cozy.files'
+    doctype: 'io.cozy.files',
+    relationships: {
+      albums: {
+        type: HasManyAlbums,
+        doctype: 'io.cozy.photos.albums'
+      }
+    }
   }
 }
